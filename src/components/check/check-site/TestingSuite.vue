9446366d06bbdf261fc61586b7ed4a78b33342ea
Mikhail K <ism1le@xaff.ru>
2018-05-27 03:58:44 +0300

    v0.0.6-rc3 - Fixen npn and waf parsing

--- src/components/check/check-site/TestingSuite.vue
+++ src/components/check/check-site/TestingSuite.vue
@@ -435,7 +435,6 @@ export default {
                 }
                 this.response = response.body;
                 clearInterval(timer);
-
                 this.finalGrade = this.response.grade;
                 this.finalGradeColor = this.getColor(this.response.score);
                 this.serverInfo.serverIp = this.response.server_ip;
@@ -447,11 +446,11 @@ export default {
                 this.resolved.shortResult = true;
                 this.webServerSecurity.httpResponse = this.response.http_response;
                 this.webServerSecurity.redirect = this.response.redirect_to === '' ? 'N/A' : this.response.redirect_to;
-                this.webServerSecurity.npn = this.response.http_additional_info.protocol_negotiation.npn ? ['Yes'] : ['No'];
+                this.webServerSecurity.npn = this.response.http_additional_info.protocol_negotiation.npn[0] === void 0 ? ['N/A'] : this.response.http_additional_info.protocol_negotiation.npn;
                 this.webServerSecurity.alpn = this.response.http_additional_info.protocol_negotiation.alpn ? 'Yes' : 'No';
                 this.webServerSecurity.contentEncoding = this.response.http_additional_info.content_encoding;
                 this.webServerSecurity.serverSignature = this.response.server_signature;
-                this.webServerSecurity.waf = this.response.http_additional_info.waf ? ['Yes'] : ['No'];
+                this.webServerSecurity.waf = this.response.http_additional_info.waf[0] === void 0 ? ['No WAF detected'] : this.response.http_additional_info.waf;
                 Object.keys(this.response.http_verbs).forEach((key, i) => {
                   this.webServerSecurity.httpMethods[i] = Object();
                   this.webServerSecurity.httpMethods[i] = {
